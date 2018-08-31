067f111e8c53e6aeb43ea147571508bfec07aed8
tw1997 <t888y@qq.com>
2018-09-01 03:06:01 +0800

    library

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'vuex-loading.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    library: 'vuexLoading',
   },
   module: {
     rules: [
