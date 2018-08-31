d8f5016323e982a7bb07d4d65aaa75f8a41f5085
tw1997 <t888y@qq.com>
2018-09-01 02:38:21 +0800

    build to dist

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'vuex-loading.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
